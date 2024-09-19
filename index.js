0eba9836564cf455998da7dd47b9774bb54ef919
Mateus <mtsmlima@gmail.com>
2024-09-19 15:25:24 -0300

    Reorganized some variables and added logic

--- index.js
+++ index.js
@@ -1,61 +1,85 @@
+const inputBill = document.getElementById('bill')
+
+const tipsPercentage = document.querySelectorAll('ul > li')
+
+const tipsCustom = document.querySelector('li#custom > input')
+
+const numberOfPeople = document.getElementById('inputPeople')
+
 const tipAmount = document.querySelector('div.amount-total p.value')
 
 const totalPerson = document.querySelector('div.price p.value')
 
 const resetButton = document.querySelector('div.tip button')
 
-const inputBill = document.getElementById('bill')
+let selectedTipPercentage = 15
 
-const numberOfPeople = document.getElementById('inputPeople')
+// Function to set initial % 15
+function setInitialPercentage() {
+   tipsPercentage.forEach(li => {
+      if (Number(li.textContent.replace('%', '')) === 15) {
+         li.classList.add('active')
+      }
+   })
+   updateDisplay()
+}
 
-const tipsPercentage = document.querySelectorAll('ul > li')
 
-const tipsCustom = document.querySelector('li#custom > input')
+function handleBillValue() {
+   inputBill.addEventListener('input', () => {
+      const billValue = Number(inputBill.value)
 
-let selectedTipPercentage = 0
+      if (billValue <= 0) {
+         inputBill.classList.add('input-custom')
+         console.log(billValue)
+         totalPerson.textContent = `$0.00`
+         tipAmount.textContent = "$0.00"
+      } else {
+         inputBill.classList.remove('input-custom')
+         updateDisplay()
+      }
+   })
+}
 
-
-// Verifies if cusinsert value if a valid number
+// Verifies if custom input value is valid
 function handleTipCustomInput(customInput) {
    if (isNaN(customInput) || customInput < 0) {
       totalPerson.textContent = `$0.00`
       tipAmount.textContent = "$0.00"
-
       tipsCustom.classList.add('input-custom')
    } else {
-      tipsCustom.classList.remove('input-custom')
       updateDisplay()
    }
 }
 
-
-tipsPercentage.forEach((liPercentage) => {
-   if (liPercentage.id === 'custom') {
-      const inputCustomPercentage = liPercentage.querySelector('#custom > input')
-
-      // Cleans tip input when focused
-      inputCustomPercentage.addEventListener('focus', () => inputCustomPercentage.value = '')
-
-      inputCustomPercentage.addEventListener('input', () => {
-         selectedTipPercentage = Number(inputCustomPercentage.value)
-
-         handleTipCustomInput(selectedTipPercentage)
-      })
-   } else {
-      // Select the other percentages
-      liPercentage.addEventListener('click', () => {
-         selectedTipPercentage = Number(liPercentage.textContent.replace('%', ''))
-
-         updateDisplay()
-      })
-   }
+tipsCustom.addEventListener('input', () => {
+   selectedTipPercentage = Number(tipsCustom.value)
+   handleTipCustomInput(selectedTipPercentage)
 })
 
-function tipCalculation() {
-   const billValue = Number(inputBill.value)
-   const peopleCount = Number(numberOfPeople.value)
+tipsCustom.addEventListener('focus', () => {
+   removeActiveClass()
+})
 
-   return billValue * (selectedTipPercentage / 100) / peopleCount
+
+tipsPercentage.forEach((liPercentage) => {
+   // Select the other percentages
+   liPercentage.addEventListener('click', () => {
+      removeActiveClass()
+
+      selectedTipPercentage = Number(liPercentage.textContent.replace('%', ''))
+
+      liPercentage.classList.add('active')
+
+      tipsCustom.value = ''
+      tipsCustom.classList.remove('input-custom')
+      updateDisplay()
+   })
+})
+
+// Remove the active class list from percentages buttons
+const removeActiveClass = () => {
+   tipsPercentage.forEach(li => li.classList.remove('active'))
 }
 
 
@@ -74,7 +98,7 @@ function ErrorMessage(showError) {
 function errors() {
    const peopleCount = Number(numberOfPeople.value)
 
-   if (!peopleCount || peopleCount <= 0) {
+   if (!peopleCount || peopleCount <= 0 || handleBillValue()) {
       ErrorMessage(true)
 
       totalPerson.textContent = `$0.00`
@@ -84,6 +108,13 @@ function errors() {
 
    ErrorMessage(false)
    return false
+}
+
+function tipCalculation() {
+   const billValue = Number(inputBill.value) || 0
+   const peopleCount = Number(numberOfPeople.value) || 0
+
+   return billValue * (selectedTipPercentage / 100) / peopleCount
 }
 
 
@@ -111,3 +142,5 @@ resetButton.addEventListener('click', resetValues);
 
 inputBill.addEventListener('input', updateDisplay)
 numberOfPeople.addEventListener('input', updateDisplay)
+
+setInitialPercentage()
